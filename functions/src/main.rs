69d4dec41a347babc8b0a3466f85b1f78436ad5a
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-11-30 13:40:27 -0600

    feat: looking at functions

---
+++ functions/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+  println!("Hello, world!");
+  let result = add(1, 2);
+  println!("1 + 2 = {}", result);
+  expression();
+  let val = return_value();
+  println!("The value of val is: {}", val);
+  let early_val = return_early();
+  println!("The value of earlyVal is: {}", early_val);
+}
+
+// must declare parameter types
+// must declare return type
+fn add(a: i32, b: i32) -> i32 {
+  a + b
+}
+
+fn expression() {
+  let _x = 5;
+  let y = {
+    let x = 3;
+    x + 1 // no semicolon
+  };
+  println!("The value of y is: {}", y);
+}
+
+// implicit return of last expression
+fn return_value() -> i32 {
+  1 + 1 // no semicolon
+}
+
+fn return_early() -> i32 {
+  return 2;
+  1 + 1
+}
